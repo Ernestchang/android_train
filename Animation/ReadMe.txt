c84c1f0dab2b1bf52000ecba00b53221f66260ec
houchen <songshanqingzi@163.com>
2016-07-14 21:22:40 +0800

    添加layout动画 添加activity跳转动画

--- Animation/ReadMe.txt
+++ Animation/ReadMe.txt
@@ -1,10 +1,25 @@
 
 Animation分类
-1. View Animation 渐变动画
-   旋转  RotateAnimation
-   移动  TranslateAnimation
-   缩放  ScaleAnimation
-   淡出  AlphaAnimation
-2. Drawable Animation 逐帧动画
-   按照固定时间间隔显示图片序列
-3. Property Animation
+1. Tween Animation      变换动画
+2. Frame Animation      逐帧动画
+3. Property Animation   属性动画
+4. Layout Animation     布局动画
+
+Tween Animation分类
+1. RotateAnimation      旋转
+2. TranslateAnimation   移动
+3. ScaleAnimation       缩放
+4. AlphaAnimation       淡出
+
+Tween Animation共同属性
+1. duration        动画持续时间（单位：毫秒）
+2. fillAfter       为true时，在动画结束时保持动画结束时状态
+3. fillBefore      为true时，在动画结束时保持动画开始前状态
+4. interpolator    动画加速度（加速、减速、先加速后减速）
+5. repeatCount     动画重复次数
+6. repeatMode      动画重复方式：顺序/倒序
+7. startOffset     动画之间的时间间隔
+
+Tween Animatioin 实现方式
+1. 配置文件（/res/anim）  alpha / scale / translate / rotate
+2. Java代码实现  AlphaAnimation / ScaleAnimation / TranslateAnimation / RotateAnimation
--- Animation/app/src/main/java/com/hc/animation/MainActivity.java
+++ Animation/app/src/main/java/com/hc/animation/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.hc.animation.frame.FrameAnimationActivity;
+import com.hc.animation.layout.LayoutAnimationActivity;
 import com.hc.animation.tween.TweenAnimationActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findViewById(R.id.btn_tween).setOnClickListener(this);
         findViewById(R.id.btn_frame).setOnClickListener(this);
+        findViewById(R.id.btn_layout).setOnClickListener(this);
     }
 
     @Override
@@ -28,11 +30,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_frame:
                 start(FrameAnimationActivity.class);
                 break;
+            case R.id.btn_layout:
+                start(LayoutAnimationActivity.class);
+                break;
         }
     }
 
     private void start(Class clz) {
         startActivity(new Intent(this, clz));
+        // 第一个参数是跳转的目的activity进入的动画，每二个参数是当前activity的退出动画
+        overridePendingTransition(R.anim.zoom_in, R.anim.zoom_out);
     }
 
 }
